b28000d4c930c360f2eef69c89954e5efdf04176
mxmchrbrt <user@T480.localdomain>
2026-04-01 12:12:45 +0200

    a

--- dwm/config.h
+++ dwm/config.h
@@ -2,16 +2,16 @@
 
 /* appearance */
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
-static const unsigned int gappx     = 10;        /* gaps between windows */
+static const unsigned int gappx     = 5;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=14" };
 static const char dmenufont[]       = "monospace:size=14";
-static const char col_gray1[]       = "#000000";
-static const char col_gray2[]       = "#ffffff";
-static const char col_gray3[]       = "#ffffff";
-static const char col_gray4[]       = "#ffffff";
+static const char col_gray1[]       = "#ffffff";
+static const char col_gray2[]       = "#000000";
+static const char col_gray3[]       = "#000000";
+static const char col_gray4[]       = "#000000";
 static const char col_cyan[]        = "#535353";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
@@ -22,14 +22,22 @@ static const char *colors[][3]      = {
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
+/* appicons */
+/* NOTE: set to 0 to set to default (whitespace) */
+static char outer_separator_beg      = '[';
+static char outer_separator_end      = ']';
+static char inner_separator          = ' ';
+static unsigned truncate_icons_after = 2; /* will default to 1, that is the min */
+static char truncate_symbol[]         = "...";
+
 static const Rule rules[] = {
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	/* class      instance    title       tags mask     isfloating   monitor   appicon*/
+	{ "Gimp",     NULL,       NULL,       0,            1,           -1,         NULL },
+	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1,         "󰈹"  },
 };
 
 /* layout(s) */
@@ -60,7 +68,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "alacritty", NULL };
+static const char *termcmd[]  = { "st", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
