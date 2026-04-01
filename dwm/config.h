1a46cb144d77043f8655a04b59db359f8a0a9144
mxmchrbrt <user@T480.localdomain>
2026-04-01 14:07:30 +0200

    a

--- dwm/config.h
+++ dwm/config.h
@@ -1,17 +1,18 @@
+#include <X11/XF86keysym.h>
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
-static const unsigned int gappx     = 5;        /* gaps between windows */
+static const unsigned int gappx     = 10;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=14" };
 static const char dmenufont[]       = "monospace:size=14";
-static const char col_gray1[]       = "#ffffff";
-static const char col_gray2[]       = "#000000";
-static const char col_gray3[]       = "#000000";
-static const char col_gray4[]       = "#000000";
+static const char col_gray1[]       = "#000000";
+static const char col_gray2[]       = "#ffffff";
+static const char col_gray3[]       = "#ffffff";
+static const char col_gray4[]       = "#ffffff";
 static const char col_cyan[]        = "#535353";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
@@ -68,7 +69,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -108,6 +109,14 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ 0,                 XF86XK_AudioLowerVolume,  spawn, SHCMD("pamixer --decrease 5") },
+    { 0,                 XF86XK_AudioRaiseVolume,  spawn, SHCMD("pamixer --increase 5") },
+    { 0,                 XF86XK_AudioMute,         spawn, SHCMD("pamixer --toggle-mute") },
+
+    /* Brightness Control */
+	{ 0,                 XF86XK_MonBrightnessDown,  spawn, SHCMD("xbacklight -dec 5") },
+	{ 0,                 XF86XK_MonBrightnessUp,    spawn, SHCMD("xbacklight -inc 5") },
+
 };
 
 /* button definitions */
