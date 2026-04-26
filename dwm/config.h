925e93055bd1b1623a116bdaf2818a27aa550e4f
mxmchrbrt <maxime.charberet@epita.fr>
2026-04-26 17:35:51 +0200

    a

--- dwm/config.h
+++ dwm/config.h
@@ -3,12 +3,12 @@
 
 /* appearance */
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
-static const unsigned int gappx     = 10;        /* gaps between windows */
+static const unsigned int gappx     = 30;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=14" };
-static const char dmenufont[]       = "monospace:size=14";
+static const char *fonts[]          = { "JetBrains Mono Light:size=14" };
+static const char dmenufont[]       = "JetBrains Mono Light:size=14";
 static const char col_gray1[]       = "#000000";
 static const char col_gray2[]       = "#ffffff";
 static const char col_gray3[]       = "#ffffff";
